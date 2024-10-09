b2e0536ce5482974c2f07056cdb953839b83c9f4
Chris Monosmith <cmmonosmith@gmail.com>
2024-10-09 14:05:20 -0700

    add gitignore for /bin and add barebones scripts for build/clean

---
+++ scripts/make.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+VERBOSE=0
+
+clean () {
+    go clean $( [ "${VERBOSE}" = "1" ] && echo "-x" )
+    rm -f bin/${project}
+}
+
+compile () {
+    go build -o bin/${project} $( [ "${VERBOSE}" = "1" ] && echo "-x -v" )
+    chmod +x bin/${project}
+}
+
+package () {
+    APP_NAME="${project}" docker build ./bin
+}
+
+build () {
+    compile
+    package    
+}
+
+#start () {
+#    stop
+#}
+
+#stop () {
+
+#}
+
+# entry point
+cd -- $( dirname -- "${BASH_SOURCE[0]}")/..
+project="${PWD##*/}"
+
+# parse args
+for arg in "$@"
+do
+    case "$arg" in
+        "--verbose")
+            VERBOSE=1
+            ;;
+        "--debug")
+            export DISCORD_BOT_LOG_DEBUG=1
+            ;;
+        *)
+            command="${command} ${arg}"
+            ;; 
+    esac
+    shift
+done
+$( echo "${command}" )
